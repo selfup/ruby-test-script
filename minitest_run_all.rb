ebf54daaa7de54b049e2825f28ea057c2ad7d837
selfup <boudinot.regis@yahoo.com>
2016-01-02 00:46:40 -0700

    no more tabbing - shows up properly on github

--- minitest_run_all.rb
+++ minitest_run_all.rb
@@ -5,10 +5,10 @@ test_map = l.split("\n")
 `cd ".."`
 
 test_map.map do |run_test|
-	if run_test == 'coverage'
-		puts "ignoring coverage folder"
-	else
-		puts "test/#{run_test}"
-		puts `ruby "test/#{run_test}"`
-	end
+  if run_test == 'coverage'
+    puts "ignoring coverage folder"
+  else
+    puts "test/#{run_test}"
+    puts `ruby "test/#{run_test}"`
+  end
 end
